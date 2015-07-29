ee5e3132847d88ef90fb403d6c03cc5bd4777fbe
Ben <ben-smith@blueyonder.co.uk>
2015-07-29 13:16:04 +0100

    Adding ability to generate a weekly planner table based on number of days and start-day, with slots for lunch and dinner

--- site/js/meals.js
+++ site/js/meals.js
@@ -6,10 +6,23 @@ $(document).ready(function() {
     //
     // Global Vars
     //
+    var days = [
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+        'Sunday',
+    ];
 
     //
     // Page Initialisation
     //
+    var startDaySelect = $('#startDay');
+    for (var dayIndex in days) {
+        startDaySelect.append($('<option>', {'value': dayIndex, html: days[dayIndex]}));
+    }
 
     //
     // Event Handler Registration
@@ -20,6 +33,7 @@ $(document).ready(function() {
     //
     $('ul#mealsList').delegate('li', 'click', mealsListClick);
     $('ul#selectedMealsList').delegate('li', 'click', selectedMealsListClick);
+    $('#btnMakePlanner').on('click', makePlanner);
 
     //
     // Callbacks
@@ -38,6 +52,44 @@ $(document).ready(function() {
 
         var mealList = $('ul#mealsList');
         mealList.append(meal);
+    }
+
+    function makePlanner()
+    {
+        var numDays  = $('#numDays').val();
+        var startDay = $('#startDay').val();
+
+        var table = $('<table>').attr('id', 'tblWeeklyPlanner');
+        var headerRow = $('<th>');
+        var dayCell    = $('<td class="dayCell">').html('Day');
+        var lunchCell  = $('<td class="lunchCell">').html('Lunch');
+        var dinnerCell = $('<td class="dinnerCell">').html('Dinner');
+
+        headerRow.append(dayCell)
+                 .append(lunchCell)
+                 .append(dinnerCell);
+
+        table.append(headerRow);
+
+
+        for (var i = startDay; i < numDays; i++) {
+            var j = i % 7;
+            var row = $('<tr>');
+            var dayCell    = $('<td class="dayCell">').html(days[j]);
+            var lunchCell  = $('<td class="lunchCell">').append($('<input>', {type: 'text'}).addClass('lunchInput'));
+            var dinnerCell = $('<td class="dinnerCell">').append($('<input>', {type: 'text'}).addClass('dinnerInput'));
+
+            row.append(dayCell)
+                .append(lunchCell)
+                .append(dinnerCell);
+
+            table.append(row);
+        }
+
+        var plannerContainer = $('#weeklyPlanner');
+        plannerContainer.find('#tblWeeklyPlanner').remove();
+        plannerContainer.append(table);
+        return false;
     }
 
     //
--- site/views/meals.php
+++ site/views/meals.php
@@ -16,3 +16,21 @@
     </ul>
 </div>
 
+<div id="weeklyPlanner">
+    <h3>Weekly Planner</h3>
+    <form>
+        <fieldset>
+            <label>
+                Start Day
+                <select id="startDay">
+                    <!-- options added dynamically -->
+                </select>
+            </label>
+            <label>
+                Number Of Days
+                <input type="text" id="numDays" />
+            </label>
+            <button id="btnMakePlanner" class="btn-small btn-primary">Make Planner</button>
+        </fieldset>
+    </form>
+</div>
