5ada008c677df2000a8dd89da0124834bb7bb44d
Ben <ben-smith@blueyonder.co.uk>
2015-08-11 12:52:40 +0100

    Adding navbar with links to Meal Planner and Meal Management pages

---
+++ site/views/nav.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-inverse" role="navigation">
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle Navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a href="#" class="navbar-brand"><?php echo $navbarTitle; ?></a>
+    </div>
+
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
+        <ul class="nav navbar-nav navbar-right">
+            <li>
+                <a href='/lista/html/planner'>Weekly Planner</a>
+            </li>
+            <li>
+                <a href='/lista/html/meals'>Meals</a>
+            </li>
+            <li>
+                <a href='/lista/html/login/logout'>Log out</a>
+            </li>
+        </ul>
+    </div>
+</nav>
